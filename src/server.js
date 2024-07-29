28d0f08c333ac1c67f79edf70115a53dbddfd780
Nicholas <100789465+nicholasjvr@users.noreply.github.com>
2024-07-29 17:07:36 +0200

    Update server.js with trigger workflows

--- src/server.js
+++ src/server.js
@@ -68,8 +68,9 @@ const updateLead = async (leadId) => {
                 {
                     'hasScratched': true
                 }
-            ]
-        }
+            ],
+            'trigger': ['workflow']
+            }
     };
 
     try {
